5816df9ba0e0fb77664bfa8884a1a444630ea9d2
Jong Hun Kim <jhukim@ucdavis.edu>
2021-08-12 18:27:58 +0900

    fix : row PY file

--- Coding_Test_Practice/row/BOJ_17143.py
+++ Coding_Test_Practice/row/BOJ_17143.py
@@ -1,93 +1,53 @@
 # Simulation
 from sys import stdin
-from collections import defaultdict
-import heapq
 R, C, M = map(int, stdin.readline().split())
-# A = [list(map(int, stdin.readline().split())) for _ in range(M)]
-dic, count = defaultdict(list), 0
+dic, count, d = [[0 for _ in range(C)] for __ in range(R)], 0, [(-1,0),(1,0),(0,1),(0,-1)]
 if M == 0:
     print(0)
     exit(0)
 for _ in range(M):
     r, c, s, d, z = map(int, stdin.readline().split())
-    heapq.heappush(dic[c], [r,z,s,d])
+    dic[r-1][c-1] = (s, d-1, z)
 
-for i in range(1, C+1):
-    print(dic)
-    if dic[i]:
-        r, z, s, d = heapq.heappop(dic[i])
-        print(i,r,z,s,d)
-        count += z
-    temp = defaultdict(list)
-    for key, value in dic.items():
-        for k in value:
-            nr, nz, ns, nd = k
-            nc = key
-            # if nd == 1:
-            #     ind = abs(nr - (ns + abs(R - ns)))
-            #     if ind > R:
-            #         nr = ind % R
-            #         nd = 1 if (ind // R) % 2 == 0 else 2
-            #     else:
-            #         nr, nd = ind, 1 if (ind // R) % 2 == 0 else 2
-            # elif nd == 2:
-            #     ind = abs(nr + (ns + abs(R - ns)))
-            #     if ind > R:
-            #         nr = ind % R
-            #         nd = 2 if (ind // R) % 2 == 0 else 1
-            #     else:
-            #         nr, nd = ind, 2 if (ind // R) % 2 == 0 else 1
-            # elif nd == 3:
-            #     ind = abs(nc + (ns + abs(C - ns)))
-            #     if ind > R:
-            #         nc = ind % R
-            #         nd = 3 if (ind // R) % 2 == 0 else 4
-            #     else:
-            #         nc, nd = ind, 3 if (ind // R) % 2 == 0 else 4
-            #     if nr == 2 and nz == 5:
-            #         print(nc)
-            # elif nd == 4:
-            #     ind = abs(nc - (ns + abs(C - ns)))
-            #     if ind > R:
-            #         nc = ind % R
-            #         nd = 4 if (ind // R) % 2 == 0 else 3
-            #     else:
-            #         nc, nd = ind, 4 if (ind // R) % 2 == 0 else 3
-            if nd < 3:
-                nns = ns % ((R - 1) * 2)
-                while nns > 0:
-                    if nd == 2:
-                        nr += 1
-                        if nr > R:
-                            nd = 4
-                            nr -= 2
-                    else:
-                        nr -= 1
-                        if nr < 1:
-                            nd = 3
-                            nr += 2
-                    nns -= 1
-            else:
-                nns = ns%((C-1)*2)
-                while nns > 0:
-                    if nd == 3:
-                        nc += 1
-                        if nc > C:
-                            nd = 4
-                            nc -= 2
-                    else:
-                        nc -= 1
-                        if nc < 1:
-                            nd = 3
-                            nc += 2
-                    nns -= 1
-                heapq.heappush(temp[nc], [nr,nz,ns,nd])
-    dic = temp
-
-    for l in dic:
-        for i in range(len(dic[l])):
-            if i+1 < len(dic[l]):
-                if dic[l][i][0] == dic[l][i+1][0]:
-                    dic[l].pop(i)
-
+for i in range(C):
+    for ind in range(R):
+        if dic[ind][i] != 0:
+            s, d, z = dic[ind][i]
+            dic[ind][i], count = 0, count + z
+            break
+    tmp = [[0 for _ in range(C)] for __ in range(R)]
+    for x in range(R):
+        for y in range(C):
+            if dic[x][y] != 0:
+                ns, nd, nz = dic[x][y]
+                nx, ny = x, y
+                if nd < 2:
+                    nns = ns % ((R - 1) * 2)
+                    for __ in range(nns):
+                        if nd == 1:
+                            nx += 1
+                            if nx >= R:
+                                nd = 0
+                                nx -= 2
+                        else:
+                            nx -= 1
+                            if nx < 0:
+                                nd = 1
+                                nx += 2
+                else:
+                    nns = ns % ((C-1)*2)
+                    for __ in range(nns):
+                        if nd == 2:
+                            ny += 1
+                            if ny >= C:
+                                nd = 3
+                                ny -= 2
+                        else:
+                            ny -= 1
+                            if ny < 0:
+                                nd = 2
+                                ny += 2
+                if tmp[nx][ny] == 0 or nz > tmp[nx][ny][2]:
+                    tmp[nx][ny] = (ns, nd, nz)
+    dic = tmp
 print(count)
